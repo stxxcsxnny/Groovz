22080b9b482c1b018e59995565d44660499e22ea
sunny <sk2993130@gmail.com>
2025-05-26 10:35:06 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-   origin: ["https://groovz-frontend-4pwze6iv7-sunnys-projects-5051540b.vercel.app","https://groovz-frontend.vercel.app"],
+   origin: ["https://groovz-frontend-4pwze6iv7-sunnys-projects-5051540b.vercel.app","https://groovz-frontend.vercel.app","https://groovzz.netlify.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
